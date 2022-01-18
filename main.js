bd01b826966a610b03967c726212f52e37ee2688
Joel Catarino <62433408+JoelCatarino@users.noreply.github.com>
2022-01-18 15:05:08 -0300

    duplication_donee

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 updateText = () => {
   let text = document.getElementById("text-input").value;
   document.getElementById("text-output").innerText = text;
+};
 
+makeBold = (elem) => {
+  elem.classList.toggle("active");
+  document.getElementById("text-output").classList.toggle("bold");
 }
-
-// updateText()
